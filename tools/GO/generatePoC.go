1b0d9e95fd7bf1fed5d7aa9bdb2f38a82827e073
nu11secur1ty <venvaropt@gmail.com>
2023-02-02 12:33:19 +0200

    Create generatePoC.go

---
+++ tools/GO/generatePoC.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+)
+
+func main() {
+	
+    f, err := os.Create("exploit.txt")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+	
+    defer f.Close()
+
+    val := `YOUR_POST_or_GET_Request_from_BurpSuite_here`
+
+    data := []byte(val)
+
+    _, err2 := f.Write(data)
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }
+		fmt.Println("done..The PoC was created, execute python exploit.py...")
+}
