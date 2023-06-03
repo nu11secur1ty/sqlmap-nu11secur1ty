d2f5ff58f7a80a83e9a8e4d8bbc57ba30426d0fc
nu11secur1ty <venvaropt@gmail.com>
2023-06-03 16:49:06 +0300

    Up
    Up

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -20,7 +20,7 @@ from thirdparty import six
 from thirdparty.six import unichr as _unichr
 
 # sqlmap version (<major>.<minor>.<month>.<monthly commit>)
-VERSION = "1.7.5.4"
+VERSION = "1.7.6.1"
 TYPE = "dev" if VERSION.count('.') > 2 and VERSION.split('.')[-1] != '0' else "stable"
 TYPE_COLORS = {"dev": 33, "stable": 90, "pip": 34}
 VERSION_STRING = "sqlmap/%s#%s" % ('.'.join(VERSION.split('.')[:-1]) if VERSION.count('.') > 2 and VERSION.split('.')[-1] == '0' else VERSION, TYPE)
@@ -36,15 +36,12 @@ ZIPBALL_PAGE = "https://github.com/sqlmapproject/sqlmap/zipball/master"
 
 # colorful banner
 BANNER = """\033[01;33m\
-                     ,d     d                                             ,d   d8           
-888-~88e 888  888 ,d888 ,d888  d88~\  e88~~8e   e88~~\ 888  888 888-~\ ,d888 _d88__ Y88b  / 
-888  888 888  888   888   888 C888   d888  88b d888    888  888 888      888  888    Y888/  
-888  888 888  888   888   888  Y88b  8888__888 8888    888  888 888      888  888     Y8/   
-888  888 888  888   888   888   888D Y888    , Y888    888  888 888      888  888      Y    
-888  888 "88_-888   888   888 \_88P   "88___/   "88__/ "88_-888 888      888  "88_/   /     
-                                                                                    _/
- \033[01;37m{\033[01;%dm%s\033[01;37m}\033[01;33m
- \033[0m\033[4;37m%s\033[0m\n https://www.nu11secur1ty.com/\n
+        ___
+       __H__
+ ___ ___[.]_____ ___ ___  \033[01;37m{\033[01;%dm%s\033[01;37m}\033[01;33m
+|_ -| . [.]     | .'| . |
+|___|_  [.]_|_|_|__,|  _|
+      |_|V...       |_|   \033[0m\033[4;37m%s\033[0m\n
 """ % (TYPE_COLORS.get(TYPE, 31), VERSION_STRING.split('/')[-1], SITE)
 
 # Minimum distance of ratio from kb.matchRatio to result in True
