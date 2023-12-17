8adb0c067620f85431528908da896a68f84aa607
nu11secur1ty <venvaropt@gmail.com>
2023-12-17 10:11:05 +0200

    Update bypass.py

--- tamper/bypass.py
+++ tamper/bypass.py
@@ -13,8 +13,8 @@ def dependencies():
 def tamper(payload, **kwargs):
 """
 Replace OR and AND keywords with || and &&
->>> tamper(' or 1=1#)
-' || or 1=1#
+>>> tamper('nu11secur1ty' or 1=1#)
+' || nu11secur1ty' or 1=1#
 """
     retVal = ""
     retVal = re.sub('\\bOR\\b', '||', payload)
