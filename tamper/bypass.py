4d13030e14df9e9c29d3ece7eafa37e54b0eb58a
nu11secur1ty <venvaropt@gmail.com>
2023-12-17 10:21:05 +0200

    Up-bypass-tamper/12/17/23
    Up-bypass-tamper/12/17/23

--- tamper/bypass.py
+++ tamper/bypass.py
@@ -5,13 +5,17 @@ Copyright (c) 2006-2023 nu11secur1ty (https://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
 
-from lib.core.enums import PRIORITY
 import re
+
+from lib.core.data import kb
+from lib.core.enums import PRIORITY
+
 __priority__ = PRIORITY.NORMAL
+
 def dependencies():
     pass
+
 def tamper(payload, **kwargs):
-"""
 Replace OR and AND keywords with || and &&
 >>> tamper('nu11secur1ty' or 1=1#)
 ' || nu11secur1ty' or 1=1#
