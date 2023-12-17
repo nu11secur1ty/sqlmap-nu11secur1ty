adcd0aa1840589070d80772faa0431c9bf0b989a
nu11secur1ty <venvaropt@gmail.com>
2023-12-17 10:06:30 +0200

    Update bypass.py

--- tamper/bypass.py
+++ tamper/bypass.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2023 nu11secur1ty (https://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
 from lib.core.enums import PRIORITY
 import re
 __priority__ = PRIORITY.NORMAL
